b4196f85e96c6488068c486ddf29fd4c5ff825dc
DeviSureshl <nitheeshkr115@gmail.com>
2024-05-28 06:31:03 +0200

    created base folder structure ans api functions for movies app

---
+++ movies/src/services/apiCall.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const getMovies = async () => {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+  const FEATURES_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=`;
+
+  try {
+    const { data } = await axios(FEATURES_API);
+    console.log(data);
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const getMovieDetails = async (id) => {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+  const FEATURES_API = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+
+  try {
+    const { data } = await axios(FEATURES_API);
+    console.log(data);
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
